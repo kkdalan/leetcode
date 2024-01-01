6d982cc9760020c1ae7c4b347b06e08173f71520
alanhuang4 <alanhuang4@fareastone.com.tw>
2024-01-01 21:27:29 +0800

    add Combinations

---
+++ src/main/java/com/alan/leetcode/medium/Combinations.java
@@ -0,0 +1,55 @@
+package com.alan.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+
+	public static void main(String[] args) {
+		Combinations sol = new Combinations();
+
+		// Case 1
+		List<List<Integer>> results = sol.combine(4, 2);
+
+		System.out.println(results);
+	}
+
+	public List<List<Integer>> combine(int n, int k) {
+
+		// create array [1...n]
+		int[] nums = new int[n];
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = i + 1;
+		}
+		
+		// collect subsets with k elements
+		List<List<Integer>> results = new ArrayList<>();
+
+		List<Integer> track = new ArrayList<>();
+
+		backtrack(nums, 0, track, results, k);
+
+		return results;
+	}
+
+	private void backtrack(int[] nums, int start, List<Integer> track, List<List<Integer>> results, int k) {
+
+		// add track into subsets when track size is k
+		if (track.size() == k) {
+			results.add(new ArrayList<>(track));
+		}
+
+		for (int i = start; i < nums.length; i++) {
+
+			// add one element
+			track.add(nums[i]);
+
+			// backtrack
+			backtrack(nums, i + 1, track, results, k);
+
+			// remove last element
+			track.remove(track.size() - 1);
+		}
+	}
+
+}
