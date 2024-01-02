4340c057b15747d8b69879686ba0b3ec834fa2ba
alanhuang4 <alanhuang4@fareastone.com.tw>
2024-01-02 23:03:07 +0800

    add TwoSumEnhance

--- src/main/java/com/alan/leetcode/easy/TwoSumEnhance.java
+++ src/main/java/com/alan/leetcode/easy/TwoSumEnhance.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class nSum {
+public class TwoSumEnhance {
 
 	public static void main(String[] args) {
 
-		nSum solution = new nSum();
+		TwoSumEnhance solution = new TwoSumEnhance();
 
 		System.out.println(Arrays.toString(solution.twoSumAsc(new int[] { 2, 7, 11, 15 }, 9)));
 		System.out.println(Arrays.toString(solution.twoSumAsc(new int[] { 2, 3, 4 }, 6)));
